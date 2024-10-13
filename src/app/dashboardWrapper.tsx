13c592fb92fe75c531be7198e06c96dd49032beb
Mohammad_dn <mohammaddehghani2677@gmail.com>
2024-10-13 12:22:57 +0330

    befor initial redux

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./(componenets)/Navbar";
+import Sidebar from "./(componenets)/Sidebar";
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
-      sidebar
+      <Sidebar />
       <main
-        className={`dark:bg-dark- dark:bg-dark-bg flex w-full flex-col bg-gray-50 md:pl-64`}
+        className={`dark:bg-dark- flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}
       >
         <Navbar />
         {children}
